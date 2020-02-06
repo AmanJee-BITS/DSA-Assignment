5c4534162ba9f756380d7b9851521b3fa953d1fd
AMANJEE <amangs896@gmail.com>
2020-02-06 18:02:28 +0530

    Add files via upload

---
+++ Assignment 4/3.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define ll long long int
+
+int a[100000];
+
+int main(){
+
+	 #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif
+
+  int n;
+  scanf("%d", &n);
+  char str[n][52];
+  int c=0;
+
+  for(int x=0; x<n; x++){
+  	scanf("%s",str[x]);
+  }
+  
+  for(int x=0; x<n; x++){
+  	for(int y=0; y<n-1; y++) {
+  		if (strlen(str[y]) < strlen(str[y+1]) && str[y][0] != '-' && str[y+1][0] != '-'){
+  			char temp[52];
+  			strcpy(temp, str[y]);
+  			strcpy(str[y], str[y+1]);
+  			strcpy(str[y+1], temp);
+  		}
+
+  		else if(strlen(str[y]) > strlen(str[y+1]) && str[y][0] == '-' && str[y+1][0] == '-'){
+  			char temp[52];
+  			strcpy(temp, str[y]);
+  			strcpy(str[y], str[y+1]);
+  			strcpy(str[y+1], temp);
+  		}
+
+  		else if(str[y][0] == '-' && str[y+1][0] != '-'){
+  			char temp[52];
+  			strcpy(temp, str[y]);
+  			strcpy(str[y], str[y+1]);
+  			strcpy(str[y+1], temp);
+  		}
+
+  		else if(strlen(str[y]) == strlen(str[y+1])) {
+  			if(str[y][0] == '-' && str[y+1][0] != '-'){
+  				char temp[52];
+  				strcpy(temp, str[y]);
+  				strcpy(str[y], str[y+1]);
+  				strcpy(str[y+1], temp);
+  			}
+
+  			else if (str[y][0] != '-' && str[y+1][0] == '-'){
+  			}
+  			else {
+  				if(str[y][0] != '-' && str[y+1][0] != '-'){
+  					for(int z = 0; z<= strlen(str[y]) -1; z++){
+  						if(str[y][z] < str[y+1][z]){
+  							char temp[52];
+  							strcpy(temp, str[y]);
+  							strcpy(str[y], str[y+1]);
+  							strcpy(str[y+1], temp);
+  							break;
+  						}
+  						else if(str[y][z] > str[y+1][z]){
+  							break;
+  						}
+  					}
+  				}
+
+  				else {
+  					for(int z = 1; z<=strlen(str[y]) - 1; z++){
+  						if(str[y][z] > str[y+1][z]){
+  							char temp[52];
+  							strcpy(temp, str[y]);
+  							strcpy(str[y], str[y+1]);
+  							strcpy(str[y+1], temp);
+  							break;
+  						}
+  						else if(str[y][z] < str[y+1][z]){
+  							break;
+  						}
+  					}
+  				}
+  			}
+  		}
+  	}
+  }
+
+  for(int x =0; x<n; x++){
+  	printf("%s\n", str[x]);
+  }
+
+  return 0;
+}
